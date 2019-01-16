3712db83b420bba1e787145a11cd1a7045bbf2a9
hindrekhan <hindrek.hannus@tptlive.ee>
2019-01-16 10:49:27 +0200

    initial commit

---
+++ WebApplication1/WebApplication1/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string AccountId { get; set; }
+        [Required, MaxLength(255)]
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public bool MarkedAsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
